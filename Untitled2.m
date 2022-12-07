b6df3df701df766b9dc4a3c379cb1fd786937a4c
lbcgi <77057559+lbcgi@users.noreply.github.com>
2022-12-07 14:02:45 +0800

    Delete Untitled2.m

--- Untitled2.m
+++
@@ -1,5 +0,0 @@
-function y = NormW32(a)   
-if (a == 0) return 0;
-v = (uint32_t) (a < 0 ? ~a : a);
-%     // returns the number of leading zero bits in the argument.
-    return (int16_t) (__clz_uint32(v) - 1);
